0784d0a2097e99d75cc109606db96a4fa6b9ca84
mangcoding <mangcoding@gmail.com>
2021-10-01 20:16:09 +0700

    remove header image on single post

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -1,11 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-  <page-header 
-  @if (get_the_post_thumbnail_url() && get_the_post_thumbnail_url() !="") 
-    image-url="{{ get_the_post_thumbnail_url(null, 'original') }}" 
-  @endif
-  >
+  <page-header>
     <h1>@php(the_title())</h1>
   </page-header>
   <div class="list-style-enable container py-16">
