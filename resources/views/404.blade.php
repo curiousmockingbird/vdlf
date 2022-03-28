2db58bbd9cdb8275d536c2967c73b282b0e5b043
Nugraha <mangcoding@gmail.com>
2022-03-28 17:33:19 +0700

    update 404 page

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -1,13 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-  @include('partials.page-header')
-
   @if (! have_posts())
-    <x-alert type="warning">
-      {!! __('Sorry, but the page you are trying to view does not exist.', 'sage') !!}
-    </x-alert>
-
-    {!! get_search_form(false) !!}
+    <page-header 
+    @if (get_the_post_thumbnail_url() && get_the_post_thumbnail_url() !="") 
+      image-url="{{ get_the_post_thumbnail_url(null, 'original') }}" 
+    @endif
+    >
+      <h1 class="text-3xl">{!! __('Sorry, but the page you are trying to view does not exist.', 'sage') !!}</h1>
+    </page-header>
+    <div class="container text-formatting-enable text-content px-5 pt-10 pb-14">
+      <div class="mt-7 mb-5">{!! get_search_form(false) !!}</div>
+    </div>
   @endif
 @endsection
