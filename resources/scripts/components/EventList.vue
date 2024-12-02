a09db2c169c5080a09e6ee29e04ae6ae2766a546
Harold Mesa <harold.mesa@icloud.com>
2024-12-02 16:46:59 -0600

    Seuccessfully fetch and display the event location object from EveryAction

--- resources/scripts/components/EventList.vue
+++ resources/scripts/components/EventList.vue
@@ -4,13 +4,21 @@
     <div v-if="loading">Loading events...</div>
     <div v-else-if="error">{{ error }}</div>
     <div v-else>
-      <div v-for="event in events" :key="event.eventId" class="event-item">
+      <div v-for="event in events" :key="event.id" class="event-item">
         <h3>{{ event.name }}</h3>
         <p>Date: {{ formatDate(event.startDate) }}</p>
-        <p v-if="event.locations && event.locations[0]?.address">
-          Location: {{ event.locations[0].address }}
+        <p>
+          Location: 
+          <span v-if="event.locations && event.locations.length > 0 && event.locations[0].address">
+            {{ event.locations[0].address.addressLine1 }},
+            {{ event.locations[0].address.city }},
+            {{ event.locations[0].address.stateOrProvince }},
+            {{ event.locations[0].address.zipOrPostalCode }}
+          </span>
+          <span v-else>
+            No location
+          </span>
         </p>
-        <p v-else>No location provided</p>
       </div>
     </div>
   </div>
@@ -36,6 +44,10 @@ export default {
         const response = await axios.get(url);
         // Extract events from the 'items' array in the response
         this.events = response.data.items || [];
+      
+      if (this.events[0]) {
+        console.log('Available properties:', Object.keys(this.events[0]));
+      }      
       } catch (err) {
         this.error = "Failed to load events. Please try again.";
         console.error(err.response || err);
@@ -51,7 +63,7 @@ export default {
   },
   mounted() {
     this.fetchEvents();
-  },
+ },
 };
 </script>
 
